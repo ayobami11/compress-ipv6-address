88a321782af89ceb961f523df05a3a7650f3c377
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2024-04-10 14:07:58 +0100

    Fixed mobile sizing issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import ToggleMode from "@/components/toggle-mode";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center p-16">
-        <div>
+      <main className="flex min-h-screen flex-col items-center">
+        <div className="py-16 w-[90%] mx-auto">
           <div className="flex gap-6 justify-between items-center mb-8">
             <h1 className="text-3xl font-bold">Compress IPv6 Address</h1>
             <ToggleMode />
